442e7b5917ed625e8ccedce96ca688e3fe840526
Maahishah <76730966+Maahishah@users.noreply.github.com>
2023-06-05 23:36:45 -0500

    Add files via upload

---
+++ train_bot.py
@@ -0,0 +1,34 @@
+# Model Training Lib
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import Adam
+
+from data_preprocessing import preprocess_train_data
+
+
+#Sequential model is appropriate for a plain stack of layers where each 
+#layer has exactly one input tensor and one output tensor.
+# we can add each using add function 
+# Relu is used for activation. 
+
+def train_bot_model(train_x, train_y):
+    model = Sequential()
+    model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(train_y[0]), activation='softmax'))
+
+
+
+# Compile Model
+model.compile(loss='categorical_crossentropy', 
+                  optimizer='adam', metrics=['accuracy'])
+
+
+
+# Fit & Save Model
+history = model.fit(train_x, train_y, epochs=10, batch_size=5, verbose=True)
+model.save('chatbot_model.h5', history)
+print("Model File Created & Saved")
+
